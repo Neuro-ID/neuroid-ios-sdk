587fd6d656b34f3a5bdde935eaed77a4ad3b8874
Clay <claytonrselby@gmail.com>
2022-05-27 12:58:28 -0500

    bump

--- NeuroID/NeuroIDTracker.swift
+++ NeuroID/NeuroIDTracker.swift
@@ -150,6 +150,7 @@ public struct NeuroID {
     }
     
     public static func getBaseURL() -> String {
+        // Prod URL
         return "https://api.neuro-id.com"
 //      return "https://rc.api.usw2-prod1.nidops.net"
 //      return "http://localhost:8080"
