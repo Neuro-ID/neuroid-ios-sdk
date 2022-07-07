0d93c915c58ed02ba1a173d8c6a1d87eab69c0a9
JosPerez <jose.perez@neuro-id.com>
2022-07-07 10:29:54 -0500

    Implement Sensor Manager
    Create Class for sensor manager and some access methods
    Create Enum with sensor types
    Create Struct to save information

--- NeuroID/NeuroIDTracker.swift
+++ NeuroID/NeuroIDTracker.swift
@@ -1653,6 +1653,7 @@ private extension UIViewController {
         var allViewControllers = self.children
         allViewControllers.append(self)
         registerSubViewsTargets(subViewControllers: allViewControllers)
+        NIDSensorManager.shared
     }
 
     @objc func neuroIDDismiss(animated flag: Bool, completion: (() -> Void)? = nil) {
