b85d8b27ec5260ab4bb9e4c35abc80e459c68f91
Collin Dunphy <collin.dunphy@neuro-id.com>
2022-07-26 14:34:00 -0400

    Create Package.swift
    You can use this file as the source of entry to edit the package.

---
+++ Package.swift
@@ -0,0 +1,45 @@
+// swift-tools-version:5.3
+//
+// Package.swift
+//
+// https://neuro-id.readme.io/docs/overview
+
+import PackageDescription
+
+let package = Package(
+    name: "Neuro-ID",
+    platforms: [.iOS(.v11)],
+    products: [
+        .library(
+            name: "NeuroID",
+            targets: ["NeuroID"]
+        )
+    ],
+    dependencies: [
+        .package(
+            name: "Alamofire",
+            url: "https://github.com/Alamofire/Alamofire.git",
+            from: "5.6.0"
+        ),
+    ],
+    targets: [
+        .target(
+            name: "NeuroID",
+            dependencies: [
+                .product(name: "Alamofire", package: "Alamofire"),
+            ],
+            path: "NeuroID",
+            exclude: ["Info.plist"]
+        ),
+        .testTarget(
+            name: "SDKTest",
+            path: "SDKTest",
+            exclude: ["Info.plist"]
+        ),
+        .testTarget(
+            name: "SDKUITest",
+            path: "SDKUITest",
+            exclude: ["Info.plist"]
+        )
+    ]
+)
