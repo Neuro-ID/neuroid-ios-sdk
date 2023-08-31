fb880d533cee91bc6f5aecbd31580d725ee85042
kevinsites <102255450+kevinsites@users.noreply.github.com>
2023-08-31 07:38:51 -0600

    ENG-6048 - Update and Add DataStore Tests (#122)
    Co-authored-by: kurt ishisaka <kurtishisaka@kurts-MacBook-Pro.local>

--- SDKTest/DataStoreTests.swift
+++ SDKTest/DataStoreTests.swift
@@ -9,67 +9,147 @@
 import XCTest
 
 class DataStoreTests: XCTestCase {
-    let eventsKey = "events_pending"
+    let clientKey = "key_live_vtotrandom_form_mobilesandbox"
 
-    func clearOutDataStore() {
-        DataStore.removeSentEvents()
-    }
+    let eventsKey = "test_events_stored"
+    let screenName = "test_screen_name"
 
     override func setUpWithError() throws {
-        UserDefaults.standard.setValue(nil, forKey: "events_ending")
-        clearOutDataStore()
+        UserDefaults.standard.setValue(nil, forKey: eventsKey)
+        NeuroID.configure(clientKey: clientKey)
         NeuroID.stop()
         NeuroID.start()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        let _ = DataStore.getAndRemoveAllEvents()
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        NeuroID.stop()
     }
 
     func testEncodeAndDecode() throws {
-        let nid1 = NIDEvent(type: .radioChange, tg: ["name": TargetValue.string("clay")], view: UIView())
+        let nidE = NIDEvent(
+            type: .radioChange,
+            tg: ["name": TargetValue.string("john")],
+            view: UIView()
+        )
 
         let encoder = JSONEncoder()
 
         do {
-            let jsonData = try encoder.encode([nid1])
+            let jsonData = try encoder.encode([nidE])
             UserDefaults.standard.setValue(jsonData, forKey: eventsKey)
             let existingEvents = UserDefaults.standard.object(forKey: eventsKey)
-            var parsedEvents = try JSONDecoder().decode([NIDEvent].self, from: existingEvents as! Data)
-            let nid2 = NIDEvent(type: .radioChange, tg: ["name": TargetValue.string("clay")], view: UIView())
-            parsedEvents.append(nid2)
-            print(parsedEvents)
+            let parsedEvents = try JSONDecoder().decode([NIDEvent].self, from: existingEvents as! Data)
+
+            assert(parsedEvents.count == 1)
+            assert(parsedEvents[0].type == "RADIO_CHANGE")
+            assert(parsedEvents[0].tg?["name"]?.toString() == "john")
         } catch {
-            print(String(describing: error))
+            assertionFailure("Failed to Encode/Decode: \(String(describing: error))")
         }
     }
 
-    func testInsertDataStore() throws {
-        // Reset the data store
-        UserDefaults.standard.setValue(nil, forKey: eventsKey)
-        let nid1 = NIDEvent(type: .radioChange, tg: ["name": TargetValue.string("clayton")], primaryViewController: LoanViewControllerPersonalDetails(), view: LoanViewControllerPersonalDetails().view)
-        DataStore.insertEvent(screen: "screen1", event: nid1)
-        let nid2 = NIDEvent(type: .radioChange, tg: ["name": TargetValue.string("bob")], primaryViewController: UIViewController(), view: UIView())
-        DataStore.insertEvent(screen: "screen2", event: nid2)
-        do {
-            let jsonData = try JSONEncoder().encode(DataStore.getAllEvents())
-            UserDefaults.standard.setValue(jsonData, forKey: eventsKey)
-            let newEvents = UserDefaults.standard.object(forKey: eventsKey)
-            let parsedEvents = try JSONDecoder().decode([NIDEvent].self, from: newEvents as! Data)
-            // Test Grouping
-            let groupedEvents = Dictionary(grouping: parsedEvents, by: { (element: NIDEvent) in
-                element.url
-            })
-            print("Events:", parsedEvents)
-            print("Grouped Events", groupedEvents)
+    func test_insertEvent_stoppedSDK() {
+        NeuroID.stop()
 
-            let radioChangeEvents = parsedEvents.filter { $0.type == "RADIO_CHANGE" }
+        let nidE = NIDEvent(
+            type: .radioChange
+        )
 
-            XCTAssert(radioChangeEvents.count == 2)
-            assert(parsedEvents.count == 4) // include CREATE_SESSION && MOBILE_METADATA event
-        } catch {
-            print(String(describing: error))
-        }
+        DataStore.insertEvent(screen: screenName, event: nidE)
+        assert(DataStore.events.count == 0)
+    }
+
+    func test_insertEvent_RNScreen() {
+        let nidE = NIDEvent(
+            type: .radioChange
+        )
+        nidE.url = "RNScreensNavigationController"
+
+        DataStore.insertEvent(screen: screenName, event: nidE)
+        assert(DataStore.events.count == 0)
+    }
+
+    func test_insertEvent_excludedView_tg() {
+        NeuroID.excludeViewByTestID(excludedView: "exclude_test_id")
+
+        let nidE = NIDEvent(
+            type: .radioChange
+        )
+        nidE.tg = [
+            "tgs": TargetValue.string("exclude_test_id"),
+        ]
+
+        DataStore.insertEvent(screen: screenName, event: nidE)
+        assert(DataStore.events.count == 0)
+    }
+
+    func test_insertEvent_excludedView_tgs() {
+        NeuroID.excludeViewByTestID(excludedView: "exclude_test_id")
+
+        let nidE = NIDEvent(
+            type: .radioChange
+        )
+
+        nidE.tgs = "exclude_test_id"
+
+        DataStore.insertEvent(screen: screenName, event: nidE)
+        assert(DataStore.events.count == 0)
+    }
+
+    func test_insertEvent_excludedView_en() {
+        NeuroID.excludeViewByTestID(excludedView: "exclude_test_id")
+
+        let nidE = NIDEvent(
+            type: .radioChange
+        )
+
+        nidE.en = "exclude_test_id"
+
+        DataStore.insertEvent(screen: screenName, event: nidE)
+        assert(DataStore.events.count == 0)
+    }
+
+    func test_insertEvent_success() {
+        let screen = "DS_TEST_SCREEN"
+        NeuroID.currentScreenName = screen
+
+        let nidE = NIDEvent(
+            type: .radioChange
+        )
+        assert(nidE.url == nil)
+
+        DataStore.insertEvent(screen: screenName, event: nidE)
+        assert(DataStore.events.count == 1)
+        assert(DataStore.events[0].url == "ios://\(screen)")
+    }
+
+    func test_getAllEvents() {
+        assert(DataStore.events.count == 0)
+
+        DataStore.events = [
+            NIDEvent(
+                type: .radioChange
+            ),
+        ]
+
+        let retrievedEvents = DataStore.getAllEvents()
+
+        assert(retrievedEvents.count == 1)
+    }
+
+    func test_getAndRemoveAllEvents() {
+        assert(DataStore.events.count == 0)
+
+        DataStore.events = [
+            NIDEvent(
+                type: .radioChange
+            ),
+        ]
+
+        let retrievedEvents = DataStore.getAndRemoveAllEvents()
+
+        assert(retrievedEvents.count == 1)
+        assert(DataStore.events.count == 0)
     }
 }
