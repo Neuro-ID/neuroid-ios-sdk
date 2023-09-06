7fed656905bdf3c27e90e232fed5786a524c8af5
Clay <claytonrselby@gmail.com>
2023-09-06 10:52:47 -0700

    [ENG-6061] Removing secure text (#125)

--- NeuroID/TrackerClassExtensions/UIClassExtensions/CommonTextFieldUtils.swift
+++ NeuroID/TrackerClassExtensions/UIClassExtensions/CommonTextFieldUtils.swift
@@ -11,14 +11,4 @@ import UIKit
 import Foundation
 
 class CommonTextFieldUtils {
-    static func isSensitiveEntry(for textField: UITextField) -> Bool {
-        if #available(iOS 11.0, *) {
-            if textField.textContentType == .password || textField.isSecureTextEntry { return true }
-        }
-        if #available(iOS 12.0, *) {
-            if textField.textContentType == .newPassword { return true }
-        }
-
-        return false
-    }
 }
