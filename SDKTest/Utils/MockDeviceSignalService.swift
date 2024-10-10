39eecf929085ed1637dc7f40fbdee62606e38eef
Kevin Sites <102255450+kevinsites@users.noreply.github.com>
2024-10-10 11:14:34 -0600

    ENG-8385 - Include ADV Lib in SDK to avoid separate repos. Conditional inclusion (#223)

--- SDKTest/Utils/MockDeviceSignalService.swift
+++ SDKTest/Utils/MockDeviceSignalService.swift
@@ -6,14 +6,9 @@
 
 import Foundation
 @testable import NeuroID
-import NeuroIDAdvancedDevice
 
 class MockDeviceSignalService: DeviceSignalService {
     var mockResult: Result<(String, Double), Error>?
-
-    func getAdvancedDeviceSignal(_ apiKey: String, completion: @escaping (Result<(String, Double), Error>) -> Void) {
-        getAdvancedDeviceSignal(apiKey, clientID: "", linkedSiteID: "", completion: completion)
-    }
 
     func getAdvancedDeviceSignal(_ apiKey: String, clientID: String?, linkedSiteID: String?, completion: @escaping (Result<(String, Double), Error>) -> Void) {
         if let result = mockResult {
