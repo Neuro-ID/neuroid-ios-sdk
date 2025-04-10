fe08e255c931cb6f9ec5ecdc8fec37c032d17040
neuroid-developer <developer@neuro-id.com>
2025-04-10 16:34:05 +0000

    Update SDK to 2.3.1

--- Source/NeuroID/NIDParamsCreator.swift
+++ Source/NeuroID/NIDParamsCreator.swift
@@ -184,7 +184,7 @@ enum ParamsCreator {
         if let bundleURL = Bundle(for: NeuroIDTracker.self).url(forResource: "NeuroID", withExtension: "bundle") {
             version = Bundle(url: bundleURL)?.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String
         } else {
-            version = getSPVersionID(version: "%%%3.4.2%%%")
+            version = getSPVersionID(version: "%%%2.3.1%%%")
         }
         return "5.ios\(NeuroID.isRN ? "-rn" : "")-adv-\(version ?? "?")"
     }
