b4efe774db62a8541f782fb7730beb60d6da1432
kurt ishisaka <kurt.ishisaka@experian.com>
2025-07-07 10:52:17 -0700

    ENG=10215 fix ios build. cannot make release PR, 3.4.6 was removed.

--- Source/NeuroID/NIDParamsCreator.swift
+++ Source/NeuroID/NIDParamsCreator.swift
@@ -184,7 +184,7 @@ enum ParamsCreator {
         if let bundleURL = Bundle(for: NeuroIDTracker.self).url(forResource: "NeuroID", withExtension: "bundle") {
             version = Bundle(url: bundleURL)?.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String
         } else {
-            version = getSPVersionID(version: "%%%3.4.6%%%")
+            version = getSPVersionID(version: "%%%3.4.5%%%")
         }
         return "5.ios\(NeuroID.isRN ? "-rn" : "")-adv-\(version ?? "?")"
     }
