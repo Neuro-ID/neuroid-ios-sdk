b3f3daa2f157f357e2459d86a4a0e3e57865d047
Collin Dunphy <collin.dunphy@neuro-id.com>
2025-10-17 14:36:11 -0400

    [ENG-10743] Update Device Model Metadata

--- Package.swift
+++ Package.swift
@@ -15,14 +15,14 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.0.0"),
         .package(name: "FingerprintPro", url: "https://github.com/fingerprintjs/fingerprintjs-pro-ios", from: "2.10.0"),
+        .package(url: "https://github.com/devicekit/DeviceKit.git", from: "4.0.0"),
         .package(name:"DSJSONSchemaValidation", url: "https://github.com/dashpay/JSONSchemaValidation", from: "2.0.7"),
         .package(name: "JSONSchema", url:"https://github.com/kylef/JSONSchema.swift", from:"0.6.0")
-
     ],
     targets: [
         .target(
             name: "NeuroID",
-            dependencies: ["Alamofire", "FingerprintPro"],
+            dependencies: ["Alamofire", "FingerprintPro", "DeviceKit"],
             path: "Source/NeuroID",
             exclude: [
                
