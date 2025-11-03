64d464490f924797f06d7b8d5021c97a4977c74e
Collin Dunphy <collin.dunphy@neuro-id.com>
2025-11-03 18:57:15 -0500

    [ENG-10804] Update FingerprintPro SDK to v2.11.0 (#324)

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.0.0"),
-        .package(name: "FingerprintPro", url: "https://github.com/fingerprintjs/fingerprintjs-pro-ios", from: "2.10.0"),
+        .package(name: "FingerprintPro", url: "https://github.com/fingerprintjs/fingerprintjs-pro-ios", from: "2.11.0"),
         .package(name:"DSJSONSchemaValidation", url: "https://github.com/dashpay/JSONSchemaValidation", from: "2.0.7"),
         .package(name: "JSONSchema", url:"https://github.com/kylef/JSONSchema.swift", from:"0.6.0")
 
