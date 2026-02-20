61679fd12c9d8d62635434f7f3cb483c40eedccf
Collin Dunphy <collin.dunphy@neuro-id.com>
2026-02-20 16:32:11 -0500

    Revert form and pin FP

---
+++ Source/NeuroID/NeuroIDClass/Extensions/NIDForm.swift
@@ -0,0 +1,37 @@
+//
+//  NIDForm.swift
+//  NeuroID
+//
+//  Created by Kevin Sites on 5/31/23.
+//
+
+import Foundation
+
+public extension NeuroID {
+    /**
+     Form Submit, Sccuess & Failure
+     */
+    @available(*, deprecated, message: "formSubmit is deprecated and no longer required")
+    static func formSubmit() -> NIDEvent {
+        let submitEvent = NIDEvent(type: NIDEventName.applicationSubmit)
+        NeuroIDCore.shared.saveEventToLocalDataStore(submitEvent)
+        NIDLog.info("**** NOTE: THIS METHOD IS DEPRECATED AND IS NO LONGER REQUIRED")
+        return submitEvent
+    }
+
+    @available(*, deprecated, message: "formSubmitFailure is deprecated and no longer required")
+    static func formSubmitFailure() -> NIDEvent {
+        let submitEvent = NIDEvent(type: NIDEventName.applicationSubmitFailure)
+        NeuroIDCore.shared.saveEventToLocalDataStore(submitEvent)
+        NIDLog.info("**** NOTE: THIS METHOD IS DEPRECATED AND IS NO LONGER REQUIRED")
+        return submitEvent
+    }
+
+    @available(*, deprecated, message: "formSubmitSuccess is deprecated and no longer required")
+    static func formSubmitSuccess() -> NIDEvent {
+        let submitEvent = NIDEvent(type: NIDEventName.applicationSubmitSuccess)
+        NeuroIDCore.shared.saveEventToLocalDataStore(submitEvent)
+        NIDLog.info("**** NOTE: THIS METHOD IS DEPRECATED AND IS NO LONGER REQUIRED")
+        return submitEvent
+    }
+}
