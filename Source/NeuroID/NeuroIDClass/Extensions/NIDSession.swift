9f145a13123b704e44b715e19d2c3ae37fe9de15
Collin Dunphy <collin.dunphy@neuro-id.com>
2026-08-11 09:56:40 -0600

    [ENG-12064] Restore `sessionID` to `SessionStartResult` (#405)

--- Source/NeuroID/NeuroIDClass/Extensions/NIDSession.swift
+++ Source/NeuroID/NeuroIDClass/Extensions/NIDSession.swift
@@ -12,6 +12,11 @@ public struct SessionStartResult {
     public let started: Bool
     public let identityId: String
 
+    @available(*, deprecated, renamed: "identityId")
+    public var sessionID: String {
+        return identityId
+    }
+
     init(_ started: Bool, _ identityId: String) {
         self.started = started
         self.identityId = identityId
